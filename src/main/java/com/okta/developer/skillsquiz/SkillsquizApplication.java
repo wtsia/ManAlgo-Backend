3c1122e644c420358f88d6a7a856def6e5f701d1
Winston <winstontsia@gmail.com>
2024-08-07 00:26:46 -0700

    init web, devtools, postgreSQL driver, data JPA

---
+++ src/main/java/com/okta/developer/skillsquiz/SkillsquizApplication.java
@@ -0,0 +1,13 @@
+package com.okta.developer.skillsquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okta/developer/skillsquiz/SkillsquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.developer.skillsquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
